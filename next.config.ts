ea3057a60db51ccbeeb60ccfe7a9d124362b0e2e
Eddie Prislac <11530791+eprislac@users.noreply.github.com>
2025-09-26 14:15:03 -0500

    Adds testing stuff

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  allowedDevOrigins: ["cbdb.mine", "*.cbdb.mine"],
+  allowedDevOrigins: ["cbdb.mine", ".cbdb.mine", "pro.local", ".pro.local"],
 };
 
 export default nextConfig;
